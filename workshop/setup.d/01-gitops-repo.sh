5135ba8fe84ea4b1ea277137cf017517d42f7be6
arslanabbasi <arslan.abbasi13@gmail.com>
2023-01-04 14:01:46 -0800

    updating setup scripts

--- workshop/setup.d/01-gitops-repo.sh
+++ workshop/setup.d/01-gitops-repo.sh
@@ -15,17 +15,17 @@ git config user.email "gitea_admin@example.com"
 git add .
 git commit -a -m "Initial Commit"
 
-git remote add origin https://gitea_admin:$GITEA_PASSWORD@gitea.${INGRESS_DOMAIN}/gitea_admin/$REPO_NAME.git
+git remote add origin https://gitea_admin:$GITEA_PASSWORD@gitea.tanzu.demo:31132/gitea_admin/$REPO_NAME.git
 git push -u origin main
 
 cd ..
-git clone https://gitea_admin:$GITEA_PASSWORD@gitea.${INGRESS_DOMAIN}/gitea_admin/gitops-workloads.git
+git clone https://gitea_admin:$GITEA_PASSWORD@gitea.tanzu.demo:31132/gitea_admin/gitops-workloads.git
 git -C /home/eduk8s/gitops-workloads config user.name gitea_admin
 git -C /home/eduk8s/gitops-workloads config user.email "gitea_admin@example.com"
 
 envsubst < workload.yaml > gitops-workloads/workload-$SESSION_NAMESPACE.yaml
 
-git clone https://gitea_admin:$GITEA_PASSWORD@gitea.${INGRESS_DOMAIN}/gitea_admin/gitops-deliverables.git
+git clone https://gitea_admin:$GITEA_PASSWORD@gitea.tanzu.demo:31132/gitea_admin/gitops-deliverables.git
 git -C /home/eduk8s/gitops-deliverables config user.name gitea_admin
 git -C /home/eduk8s/gitops-deliverables config user.email "gitea_admin@example.com"
 
--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -3,4 +3,4 @@ set -x
 set +e
 
 REGISTRY_USER=${HARBOR_USER:-admin}
-REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.${INGRESS_DOMAIN} --registry-user $REGISTRY_USER
+REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry 192.168.49.2:30002 --registry-user $REGISTRY_USER
